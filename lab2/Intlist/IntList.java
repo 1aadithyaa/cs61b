5af5b2219506fdde95aad2b7f8c24cc7dafe0d00
Aadithyaa <1aadithyaa@gmail.com>
2021-04-03 17:33:26 -0700

    jo

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -81,12 +81,10 @@ public class IntList {
      */
 
     public static IntList dcatenate(IntList A, IntList B) {
-<<<<<<< HEAD
         if(A == null || B == null) {
             throw new NullPointerException("Arguments cannot be null");
         }
-=======
->>>>>>> master
+
         IntList temp = A;
         while(A.rest != null) {
             A = A.rest;
@@ -100,12 +98,11 @@ public class IntList {
      * * elements of B.  May NOT modify items of A.  Use 'new'.
      */
     public static IntList catenate(IntList A, IntList B) {
-<<<<<<< HEAD
+
         if(A == null || B == null) {
             throw new NullPointerException("Arguments cannot be null");
         }
-=======
->>>>>>> master
+
         IntList a_res = new IntList(A.first, null);
         IntList b_res = new IntList(B.first, null);
         IntList a_ptr = a_res;
