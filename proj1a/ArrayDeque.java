01c658d78d7e8696546bf04b1cd0dfa818fa673d
Aadithyaa <1aadithyaa@gmail.com>
2021-04-07 21:24:17 -0700

    Added a lot of stuff

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -1,2 +1,84 @@
-package PACKAGE_NAME;public class ArrayDeque {
+public class ArrayDeque<Item> {
+    /**
+     * INVARIANTS
+     * front + 1 is the index of the first item of the list
+     * back -1 is the index of the last item of the list
+     */
+    private Item[] items;
+    private int size;
+    private int front;
+    private int back;
+
+
+    public ArrayDeque() {
+        items = (Item[]) new Object[100];
+        size = 0;
+        front = 49;
+        back = 51;
+    }
+
+    public void resizeFront() {
+        Item[] temp = (Item[]) new Object[size*2];
+        System.arraycopy(items, 0, temp, size, size);
+        front = size - 1;
+    }
+
+    public void resizeBack() {
+        Item[] temp = (Item[]) new Object[size*2];
+        System.arraycopy(items, 0, temp, 0, size);
+        back = size;
+    }
+    public void addFirst(Item item) {
+        if(front == -1) {
+            resizeFront();
+        }
+        items[front] = item;
+        front -= 1;
+        size += 1;
+
+    }
+
+    public void addLast(Item item) {
+        if(back == items.length-1) {
+            resizeBack();
+        }
+        items[back] = item;
+        back += 1;
+        size += 1;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for(int i = front + 1; i < size; i += 1) {
+            System.out.print(get(i) + " ");
+        }
+    }
+
+    public Item removeFirst() {
+        Item ret = get(front + 1);
+        items[front+1] = null;
+        front += 1;
+        size -= 1;
+        return ret;
+    }
+
+    public Item removeLast() {
+        Item ret = get(back - 1);
+        items[back - 1] = null;
+        back -= 1;
+        size -= 1;
+        return ret;
+
+    }
+
+    public Item get(int index) {
+        return items[front + 1 + index];
+    }
 }
--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,2 +1,154 @@
-package PACKAGE_NAME;public class LinkedListDeque {
+public class LinkedListDeque<Item> {
+
+    //Helper class that will hide the naked recursive data structure
+    private class Node {
+        private Item item;
+        private Node next;
+        private Node prev;
+
+        public Node() {
+            item = null;
+            this.next = null;
+            this.prev = null;
+        }
+
+    }
+    private final Node sentinel;
+    private int size;
+
+
+
+    /**
+     * Add and remove operations CANNOT involve any looping or recursion
+     * Must take constant time
+     * Get operation must use iteration
+     * Size must take constant time
+     * Don't maintain references to items no longer in the deque
+     */
+
+    /**
+     * INVARIANTS:
+     * sentinel.next will always reference the first element of the LinkedListDeque
+     * sentinel.prev will always reference the last element of the LinkedListDeque
+     * size will always hold the number of non-sentinel items in the list
+     */
+
+
+    public LinkedListDeque() {
+        sentinel = new Node();
+        sentinel.next = sentinel;
+        sentinel.prev = sentinel;
+        size = 0;
+    }
+
+
+    public void addFirst(Item item) {
+        sentinel.next = new Node();
+        sentinel.next.item = item;
+        sentinel.next.prev = sentinel;
+
+        /**
+         * If the first element added is the only element in the list, sentinel.prev now points to it
+         * This keeps sentinel.prev pointing to the last item in the list
+         */
+
+
+        if(size == 0) {
+            sentinel.prev = sentinel.next;
+        }
+
+        size += 1;
+
+    }
+
+    public void addLast(Item item) {
+        Node last = sentinel.prev;
+        Node newLast = new Node();
+        last.next = newLast;
+        newLast.item = item;
+        newLast.prev = last;
+
+        sentinel.prev = newLast;
+        size += 1;
+
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public void printDeque() {
+        for(int i = 0; i < size; i++) {
+            System.out.print(get(i) + " ");
+        }
+    }
+
+    public Item removeFirst() {
+        Node first = sentinel.next;
+        Node second = first.next;
+        sentinel.next = second;
+        second.prev = sentinel;
+
+        size -= 1;
+
+        return first.item;
+    }
+
+    public Item removeLast() {
+        Node last = sentinel.prev;
+        Node secondToLast = last.prev;
+
+        sentinel.prev = secondToLast;
+        secondToLast.next = null;
+
+        size -= 1;
+
+        return last.item;
+    }
+
+    public Item get(int index) {
+        Item ret = null;
+        //If the item is in the back half of the list, you only need to search at most half the list
+        //You can do this by starting from the last node, which sentinel.prev points to
+        if(index > size/2) {
+            Node p = sentinel.prev;
+            int i = size - 1 - index;
+            while(i > 0) {
+                p = p.prev;
+                i--;
+            }
+            ret = p.item;
+        }
+
+        else {
+            Node p = sentinel.next;
+            int i = 0;
+            while(i < index) {
+                p = p.next;
+                i++;
+            }
+            ret = p.item;
+        }
+        return ret;
+    }
+
+    public Item getRecursive(int index) {
+        Node p = getRecursiveHelper(index);
+        return p.item;
+    }
+
+    public Node getRecursiveHelper(int index) {
+        if(index == 0) {
+            return sentinel.next;
+        }
+        else {
+            Node p = getRecursiveHelper(index-1);
+            return p.next;
+        }
+    }
+
 }
