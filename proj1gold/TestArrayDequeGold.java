253d24ac8e7fc6c1ad7388ab8e404745352882a2
Aadithyaa <1aadithyaa@gmail.com>
2021-06-18 10:26:40 -0700

    Completed project 1g

--- proj1gold/TestArrayDequeGold.java
+++ proj1gold/TestArrayDequeGold.java
@@ -1,2 +1,53 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class TestArrayDequeGold {
+    @Test
+    public void testDeque() {
+        StudentArrayDeque<Integer> sad = new StudentArrayDeque<>();
+        ArrayDequeSolution<Integer> ads = new ArrayDequeSolution<>();
+        int i = 0;
+        String output = "";
+        while (true) {
+            double numberBetweenZeroAndOne = StdRandom.uniform();
+            output += "\n";
+            if (numberBetweenZeroAndOne < 0.2) {
+                sad.addLast(i);
+                ads.addLast(i);
+                output += "addLast(" + i + ")";
+            } else if (numberBetweenZeroAndOne < 0.4) {
+                sad.addFirst(i);
+                ads.addFirst(i);
+                output += "addFirst(" + i + ")";
+            }
+            else if (numberBetweenZeroAndOne < 0.6) {
+                output += "removeFirst()";
+                if (sad.removeFirst() != null || ads.removeFirst() != null) {
+                    assertEquals(output, sad.removeFirst(), ads.removeFirst());
+                }
+                else {
+                    continue;
+                }
+            }
+            else if (numberBetweenZeroAndOne < 0.8) {
+                output += "removeLast()";
+                if (sad.removeLast() != null || ads.removeLast() != null) {
+                    assertEquals(output, sad.removeLast(), ads.removeLast());
+                }
+                else {
+                    continue;
+                }
+            }
+            else {
+                output += "removeFirst()";
+                if (sad.removeFirst() != null && ads.removeFirst() != null) {
+                    assertEquals(output, sad.removeFirst(), ads.removeFirst());
+                }
+                else {
+                    continue;
+                }
+            }
+            i += 1;
+        }
+    }
 }
